8586d82e4883cb5d7d9f5b04453672f1595aff3b
Mikha38 <oyks1379@yandex.ru>
2023-05-29 22:07:01 +0300

    Анимация

--- pages/Royal_Coins.tsx
+++ pages/Royal_Coins.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
-import Image from "next/image";
 import pick1 from '../public/images/royal_coins/pick1.png'
 import pick2 from '../public/images/royal_coins/pick2.png'
 import pick3 from '../public/images/royal_coins/pick3.png'
@@ -10,8 +9,17 @@ import pick6 from '../public/images/royal_coins/pick6.png'
 import pick7 from '../public/images/royal_coins/pick7.png'
 import pick8 from '../public/images/royal_coins/pick8.png'
 import pick9 from '../public/images/royal_coins/pick9.png'
+import { useState } from "react";
+import { motion } from "framer-motion"
+
+const images = [ pick1, pick2, pick3, pick4, pick5, pick6, pick7, pick8, pick9 ];
 
 export default function Royal_Coins(){
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const nextImage = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
     return (
         <div className="relative">
             <div className="absolute w-full h-full bg-[url('/images/royal_coins/bg-royal_coins.png')] bg-no-repeat bg-cover">
@@ -19,25 +27,27 @@ export default function Royal_Coins(){
             </div>
             <div className="relative flex flex-col min-h-screen">
                 <Header back/>
-                <div className="flex-grow w-auto flex justify-center py-2 ">
-                    <div className="p-5 flex justify-between rounded-[20px] border-[1px] border-[#FFE600] gap-x-6 bg-black bg-opacity-75">
-                        <div className="flex flex-col justify-between">
-                            <Image src={pick1} alt="pick"/>
-                            <Image src={pick2} alt="pick"/>
-                            <Image src={pick3} alt="pick"/>
-                        </div>
-                        <div className="flex flex-col justify-between">
-                            <Image src={pick4} alt="pick"/>
-                            <Image src={pick5} alt="pick"/>
-                            <Image src={pick6} alt="pick"/>
-                        </div>
-                        <div className="flex flex-col justify-between">
-                            <Image src={pick7} alt="pick"/>
-                            <Image src={pick8} alt="pick"/>
-                            <Image src={pick9} alt="pick"/>
+
+                <div className=" flex-grow w-auto flex justify-center py-2 ">
+                    <div className="flex flex-col justify-center">
+                        <div className="overflow-hidden h-[430px] p-5 flex justify-between rounded-[20px] border-[1px] border-[#FFE600] gap-x-6 gap-y-6 bg-black bg-opacity-75">
+                            
+                            <div className="flex flex-col justify-between">
+                                {images.map((el, i) => <motion.img key={i}  src={el.src} alt="pick" className="my-2"/>)}
+                            </div>
+
+                            <div className="flex flex-col justify-between">
+                                {images.map(el => <img src={el.src} alt="pick" className="my-2"/>)}
+                            </div>
+
+                            <div className="flex flex-col justify-between">
+                                {images.map(el => <img src={el.src} alt="pick" className="my-2"/>)}
+                            </div>
+
                         </div>
                     </div>
                 </div>
+
                 <Footer />
             </div>
         </div>
